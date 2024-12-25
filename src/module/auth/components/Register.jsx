8b20f9b6953055aede43826bbdc8a126f6c10d24
Chhun_Natharith <johnathanrith@gmail.com>
2024-12-25 15:13:35 +0700

    PG

--- src/module/auth/components/Register.jsx
+++ src/module/auth/components/Register.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import CustomSearch from "../../widget/components/CustomSearch";
 import { HiOutlineMail } from "react-icons/hi";
 import { Link } from "react-router-dom";
@@ -13,14 +13,19 @@ import { PiPasswordBold } from "react-icons/pi";
 
 const Register = () => {
     const { createUser, SignInWithGoogle, OnRegister } = useAuth();
+    const [isInAppBrowser, setIsInAppBrowser] = useState(false);
+
     useEffect(() => {
-        // Detect if the browser is an in-app browser
-        const isInAppBrowser = /FBAN|FBAV|Instagram/.test(navigator.userAgent);
-        if (isInAppBrowser) {
-            // Redirect to open in an external browser
-            window.location.href = "https://cinetime.vercel.app/";
-        }
+        // Check if the user is in an in-app browser
+        const userAgent = navigator.userAgent || navigator.vendor;
+        setIsInAppBrowser(/FBAN|FBAV|Instagram/.test(userAgent));
     }, []);
+
+    const openInBrowser = () => {
+        const url = "https://cinetime.vercel.app";
+        window.open(url, "_blank"); // Opens in the default browser
+    };
+
     // Validation schema
     const validationSchema = Yup.object({
         email: Yup.string().email("Invalid email address").required("Email is required"),
@@ -55,109 +60,121 @@ const Register = () => {
     });
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <CustomSearch
-                className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
-                name="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                placeholder="Email"
-                icon={<HiOutlineMail />}
-                type="email"
-            />
-            {formik.touched.email && formik.errors.email && (
-                <p className="text-red-500">{formik.errors.email}</p>
-            )}
+        <div>
+            {isInAppBrowser ? (
+                <div>
+                    <h1>Sign-In Issue</h1>
+                    <p>
+                        It seems you're using an in-app browser. Please open this link in
+                        your default browser for a better experience.
+                    </p>
+                    <button onClick={openInBrowser}>Open in Browser</button>
+                </div>
+            ) : <form onSubmit={formik.handleSubmit}>
+                <CustomSearch
+                    className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    placeholder="Email"
+                    icon={<HiOutlineMail/>}
+                    type="email"
+                />
+                {formik.touched.email && formik.errors.email && (
+                    <p className="text-red-500">{formik.errors.email}</p>
+                )}
 
-            <CustomSearch
-                className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
-                name="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                placeholder="Password"
-                icon={<PiPasswordBold />}
-                type="password"
-            />
-            {formik.touched.password && formik.errors.password && (
-                <p className="text-red-500">{formik.errors.password}</p>
-            )}
+                <CustomSearch
+                    className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
+                    name="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    placeholder="Password"
+                    icon={<PiPasswordBold/>}
+                    type="password"
+                />
+                {formik.touched.password && formik.errors.password && (
+                    <p className="text-red-500">{formik.errors.password}</p>
+                )}
 
-            <CustomSearch
-                className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
-                name="age"
-                value={formik.values.age}
-                onChange={formik.handleChange}
-                placeholder="Age"
-                icon={<FaPerson />}
-                type="number"
-            />
-            {formik.touched.age && formik.errors.age && (
-                <p className="text-red-500">{formik.errors.age}</p>
-            )}
+                <CustomSearch
+                    className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
+                    name="age"
+                    value={formik.values.age}
+                    onChange={formik.handleChange}
+                    placeholder="Age"
+                    icon={<FaPerson/>}
+                    type="number"
+                />
+                {formik.touched.age && formik.errors.age && (
+                    <p className="text-red-500">{formik.errors.age}</p>
+                )}
 
-            <CustomSearch
-                className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
-                name="name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                placeholder="Name"
-                icon={<LuUserRound />}
-                type="text"
-            />
-            {formik.touched.name && formik.errors.name && (
-                <p className="text-red-500">{formik.errors.name}</p>
-            )}
+                <CustomSearch
+                    className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
+                    name="name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    placeholder="Name"
+                    icon={<LuUserRound/>}
+                    type="text"
+                />
+                {formik.touched.name && formik.errors.name && (
+                    <p className="text-red-500">{formik.errors.name}</p>
+                )}
 
-            <CustomSearch
-                className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
-                name="nation"
-                value={formik.values.nation}
-                onChange={formik.handleChange}
-                placeholder="Nation"
-                icon={<CiFlag1 />}
-                type="text"
-            />
-            {formik.touched.nation && formik.errors.nation && (
-                <p className="text-red-500">{formik.errors.nation}</p>
-            )}
+                <CustomSearch
+                    className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
+                    name="nation"
+                    value={formik.values.nation}
+                    onChange={formik.handleChange}
+                    placeholder="Nation"
+                    icon={<CiFlag1/>}
+                    type="text"
+                />
+                {formik.touched.nation && formik.errors.nation && (
+                    <p className="text-red-500">{formik.errors.nation}</p>
+                )}
 
-            <CustomSearch
-                className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
-                name="phone"
-                value={formik.values.phone}
-                onChange={formik.handleChange}
-                placeholder="Phone"
-                icon={<MdOutlineLocalPhone />}
-                type="text"
-            />
-            {formik.touched.phone && formik.errors.phone && (
-                <p className="text-red-500">{formik.errors.phone}</p>
-            )}
+                <CustomSearch
+                    className="flex items-center border-b active:border-b transition-all hover:border-b my-5"
+                    name="phone"
+                    value={formik.values.phone}
+                    onChange={formik.handleChange}
+                    placeholder="Phone"
+                    icon={<MdOutlineLocalPhone/>}
+                    type="text"
+                />
+                {formik.touched.phone && formik.errors.phone && (
+                    <p className="text-red-500">{formik.errors.phone}</p>
+                )}
 
-            <button className="btn w-full text-lg" type="submit">
-                Sign-Up
-            </button>
+                <button className="btn w-full text-lg" type="submit">
+                    Sign-Up
+                </button>
 
-            <div className="my-5">
+                <div className="my-5">
         <span className="font-light mb-5 flex">
           <p className="pe-3">Don't have an account?</p>
           <Link to="/auth/login">
             <p className="font-bold">Login</p>
           </Link>
         </span>
-                <Link
-                    className="flex items-center btn-outline btn hover:bg-white hover:text-black text-white mt-5 border-1"
-                    to="/"
-                    onClick={SignInWithGoogle}
-                >
-                    <img
-                        src="https://img.icons8.com/color/48/000000/google-logo.png"
-                        alt="Google Logo"
-                    />
-                    <span>Continue with Google</span>
-                </Link>
-            </div>
-        </form>
+                    <Link
+                        className="flex items-center btn-outline btn hover:bg-white hover:text-black text-white mt-5 border-1"
+                        to="/"
+                        onClick={SignInWithGoogle}
+                    >
+                        <img
+                            src="https://img.icons8.com/color/48/000000/google-logo.png"
+                            alt="Google Logo"
+                        />
+                        <span>Continue with Google</span>
+                    </Link>
+                </div>
+            </form>}
+        </div>
+
     );
 };
 
